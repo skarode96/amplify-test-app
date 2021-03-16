53351442885931bf45555cbf0e4ac07d10572674
skarode96 <skarode96@gmail.com>
2021-03-16 12:12:38 +0000

    adding env var

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h2>Environment: {process.env.NODE_ENV}</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
